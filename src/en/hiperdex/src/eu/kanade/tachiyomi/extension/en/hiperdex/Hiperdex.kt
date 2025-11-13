21d18ef25d877ef3b71c02ed29f2edeade420969
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-11-13 12:55:11 +0700

    HiperDex: Fix chapter list (#11612)
    Fix chapter list

--- src/en/hiperdex/src/eu/kanade/tachiyomi/extension/en/hiperdex/Hiperdex.kt
+++ src/en/hiperdex/src/eu/kanade/tachiyomi/extension/en/hiperdex/Hiperdex.kt
@@ -129,6 +129,8 @@ class Hiperdex :
 
     override fun searchMangaSelector() = "#loop-content div.page-listing-item"
 
+    override val chapterUrlSuffix = ""
+
     override fun mangaDetailsParse(document: Document): SManga {
         return super.mangaDetailsParse(document).apply {
             val cleanedTitle = title.cleanTitleIfNeeded()
