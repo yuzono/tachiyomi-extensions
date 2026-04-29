0d0fa261097fbf50be2abc8e93c02adff72331c9
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-04-29 12:28:38 +0700

    MissKon: Support related entries (#15231)
    (cherry picked from commit ff0a60e5126125b771caf03e6d4cf14a5ca16749)

--- src/all/misskon/src/eu/kanade/tachiyomi/extension/all/misskon/MissKon.kt
+++ src/all/misskon/src/eu/kanade/tachiyomi/extension/all/misskon/MissKon.kt
@@ -113,6 +113,18 @@ class MissKon : HttpSource() {
             }
         }
     }
+
+    /* Related titles */
+    override fun relatedMangaListParse(response: Response): List<SManga> {
+        val document = response.asJsoup()
+        return document.select(".content > .yarpp-related a.yarpp-thumbnail").map { element ->
+            SManga.create().apply {
+                setUrlWithoutDomain(element.attr("abs:href"))
+                title = element.attr("title")
+                thumbnail_url = element.selectFirst("img")?.absUrl("data-src")
+            }
+        }
+    }
     // endregion
 
     // region Pages
