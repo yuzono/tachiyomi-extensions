c9aeadc9bc5730ac59048ea9c3278f13bcc0d869
aNaruto <r.angkamkusuma@gmail.com>
2026-05-01 04:19:31 +0800

    fix komikdewasaart:reserve chapters

--- src/id/komikdewasaart/src/eu/kanade/tachiyomi/extension/id/komikdewasaart/KomikDewasaArt.kt
+++ src/id/komikdewasaart/src/eu/kanade/tachiyomi/extension/id/komikdewasaart/KomikDewasaArt.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.extension.id.komikdewasaart
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.SChapter
+import okhttp3.Response
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -11,4 +13,6 @@ class KomikDewasaArt :
         "id",
         mangaUrlDirectory = "/komik",
         dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
-    )
+    ) {
+    override fun chapterListParse(response: Response): List<SChapter> = super.chapterListParse(response).reversed()
+}
