aee7e6f121857e0fa11b96a676bd0e82993d2a19
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-05-17 00:25:36 +0700

    Update Project name to "Yuzono-Manga" (#423)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ dependencyResolutionManagement {
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-rootProject.name = "Keiyoushi"
+rootProject.name = "Yuzono-Manga"
 
 /**
  * Add or remove modules to load as needed for local development here.
