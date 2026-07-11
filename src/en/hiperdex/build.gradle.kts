7387532f6391bb0ffe51f72c22327e96383fc47f
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-07-12 00:09:02 +0700

    Bump version

--- src/en/hiperdex/build.gradle.kts
+++ src/en/hiperdex/build.gradle.kts
@@ -4,11 +4,10 @@ plugins {
 
 keiyoushi {
     name = "Hiperdex"
-    versionCode = 81
+    versionCode = 82
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "hiper"
-    kmkVersionCode = 1
 
     source {
         lang = "en"
